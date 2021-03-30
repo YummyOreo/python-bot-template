a8cb77c62122cb9c3e82f9cbe0d3a73d6a4bf2aa
OreoDivision <Bobgim20@gmail.com>
2021-03-30 08:00:42 -0500

    Adds the framwork of the bot

---
+++ main.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+import os, time
+from dotenv import dotenv_values
+
+#Loads the .env file
+config = dotenv_values(".env")
+
+#Inits the bot
+bot = commands.Bot(command_prefix=config["PREFIX"])
+bot.remove_command("help") #Removes the set help command
+
+
+@bot.event
+async def on_ready():
+	#Gets the local time
+	localtime = time.asctime(time.localtime(time.time()))
+	#Prints when ready
+	print(f"**[{localtime}]** Main file online`")
+	#changes the activity
+	await bot.change_presence(activity=discord.Game(name=config["ACTIVITY"]))
+
+#Sets the var called directory
+directory = r'.\cogs'
+#Loads in all of the cogs
+for filename in os.listdir(directory):
+	#Chacks if its a .py file
+	if filename.endswith(".py"):
+		#Logs it
+		print(f'[Loading] {filename[:-3]}...')
+		#Loads it
+		bot.load_extension(f'cogs.{filename[:-3]}')
+		#logs that its loaded
+		print(f'[Loaded] {filename[:-3]}')
+	else:
+		#Logs the fail
+		print(f'[Faile] Failed to load {filename}')
+
+bot.run(config('TOKEN'))
